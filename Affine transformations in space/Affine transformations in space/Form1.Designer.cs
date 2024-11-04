bedba2e6cfc5865b8a6d3988d1260b42c163f052
Tempire <danil.varlamov.03@mail.ru>
2024-11-04 21:40:12 +0300

    create isometric proection method and build polyhedron

--- Affine transformations in space/Affine transformations in space/Form1.Designer.cs
+++ Affine transformations in space/Affine transformations in space/Form1.Designer.cs
@@ -35,6 +35,9 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(173, 65);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1280, 575);
@@ -44,6 +47,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.Location = new System.Drawing.Point(705, 673);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(235, 54);
--- Affine transformations in space/Affine transformations in space/Form1.cs
+++ Affine transformations in space/Affine transformations in space/Form1.cs
@@ -15,20 +15,21 @@ namespace Affine_transformations_in_space
     {
 
         polyhedron pop;
+        public static int scale = 100;
+        
+        public static double focalLength = 200; //Глубина
         public Form1()
         {
             InitializeComponent();
-            
-    
+               
         }
 
 
         public void DrawTetrahedron()
         {
-            pop =  polyhedron.drawTetraedr();
+            pop =  polyhedron.drawGexaedr();
             pictureBox1.Invalidate();
-
-           
+        
         }
 
 
@@ -67,29 +68,80 @@ namespace Affine_transformations_in_space
             }
 
             public static polyhedron drawTetraedr()
-            {
-
-                point v1 = new point(0, 0, 0);
-                point v2 = new point(0, 0, 0);
-                point v3 = new point(0, 0, 0);
-                point v4 = new point(0, 0, 0);
+            {       
+                point v1 = new point(1 * scale, 1 * scale, 1 * scale);
+                point v2 = new point(1 * scale, -1 * scale, -1 * scale);
+                point v3 = new point(-1 * scale, 1 * scale, -1 * scale);
+                point v4 = new point(-1 * scale, -1 * scale, 1 * scale) ;
 
                 polygon firstPol = new polygon(new List<point> { v1, v2, v3 });
                 polygon secondPol = new polygon(new List<point> { v1, v2, v4 });
                 polygon thirdPol = new polygon(new List<point> {v1, v3, v4 });
                 polygon fourthPol = new polygon(new List<point> {v2, v3, v4 });
 
-                return new polyhedron(new List<point> { v1, v2, v3, v4 }, new List<polygon> {firstPol, secondPol, thirdPol, fourthPol });
-                
+                return new polyhedron(new List<point> { v1, v2, v3, v4 }, new List<polygon> {firstPol, secondPol, thirdPol, fourthPol });             
+            }
+
+            public static polyhedron drawGexaedr() 
+            {
+                point v1 = new point(-1 * scale, -1 * scale, -1 * scale);
+                point v2 = new point(1 * scale, -1 * scale, -1 * scale);
+                point v3 = new point(-1 * scale, 1 * scale, -1 * scale);
+                point v4 = new point(1 * scale, 1 * scale, -1 * scale);              
+                point v5 = new point(-1 * scale, -1 * scale,  1 * scale);
+                point v6 = new point(1 * scale, -1 * scale, 1 * scale);
+                point v7 = new point(-1 * scale, 1 * scale, 1 * scale);
+                point v8 = new point(1 * scale, 1 * scale, 1 * scale);
+
+                polygon firstPol = new polygon(new List<point> { v1, v2, v4, v3 }); // Нижняя грань
+                polygon secondPol = new polygon(new List<point> { v5, v6, v8, v7 }); // Верхняя грань
+                polygon thirdPol = new polygon(new List<point> { v1, v3, v7, v5 }); // Левая грань
+                polygon fourdPol = new polygon(new List<point> { v2, v4, v8, v6 }); // Правая грань
+                polygon fivethPol = new polygon(new List<point> { v1, v2, v6, v5 }); // Передняя грань
+                polygon sixthPol = new polygon(new List<point> { v3, v4, v8, v7 });  // Задняя грань
+
+                return new polyhedron(new List<point> { v1, v2, v3, v4, v5, v6, v7, v8 }, new List<polygon> {firstPol, secondPol, thirdPol, fourdPol, fivethPol, sixthPol });
             }
         }
 
+        //перспективная проекция
+        private Point PointTo2D(point p) 
+        {
+            int x2D = (int)(focalLength * p.X / (p.Z + focalLength));
+            int y2D = (int)(focalLength * p.Y / (p.Z + focalLength));
 
+            return new Point(x2D, y2D);
+        }
+
+        //Изометрическая проекция
+        private Point Isometric2DPoint(point p ) 
+        {
+            double angleCos = Math.Cos(Math.PI / 6);
+            double angleSin = Math.Sin(Math.PI / 6);
+
+            int x2D = (int)((p.X - p.Z) * angleCos);
+            int y2D = (int)((p.Y - (p.X + p.Z) * angleSin) * -1);
+
+            x2D += pictureBox1.Width / 2;
+            y2D += pictureBox1.Height / 2;
+
+            return new Point(x2D,y2D);
+        }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            if(pop != null)
-            MessageBox.Show(pop.Faces.Count.ToString());
+            if(pop != null) 
+            {
+               
+                for (int i = 0; i < pop.Faces.Count(); i++)
+                {
+                  
+                    Point[] points2D = pop.Faces[i].Vertices.Select(v => Isometric2DPoint(v)).ToArray();
+                    e.Graphics.DrawPolygon(Pens.Black, points2D);                   
+                }
+            }            
+            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
